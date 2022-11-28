d6cc5d4ffa4865622e7e70435db3a024c4e9230a
Nanang Puji <nanangpuji762@gmail.com>
2022-11-28 17:31:59 +0700

    add console and case 1 sampai 4

--- StackUsingSingleLinkList.node/Program.cs
+++ StackUsingSingleLinkList.node/Program.cs
@@ -60,6 +60,46 @@ class stack
             }
         }
     }
+    static void Main(string[] args)
+    {
+        stack s = new stack();
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("\n**tack menu**\n");
+            Console.WriteLine("1. push");
+            Console.WriteLine("2. pop ");
+            Console.WriteLine("3. display ");
+            Console.WriteLine("4. Exit ");
+            Console.Write("\nenter your Choise: ");
+            string input = Console.ReadLine();
+            char ch = Convert.ToChar(input == "" ? "0" : input);
+            switch (ch)
+            {
+                case '1':
+                    Console.Write("\n Enter a number :");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    s.push(num);
+                    break;
+                case '2':
+                    if (s.empty())
+                    {
+                        Console.WriteLine("\n Stack Empty");
+                        break;
+                    }
+                    s.pop();
+                    break;
+                case '3':
+                    s.display();
+                    break;
+                case '4':
+                    return;
+                default:
+                    Console.WriteLine("\n invalid choice");
+                    break;
+            }
+        }
+    }
 }
 
 
