04736089b53b9c4734329e0f6cd89a211bd9455a
Nanang Puji <nanangpuji762@gmail.com>
2022-11-28 17:01:39 +0700

    Add project files.

---
+++ StackUsingSingleLinkList.node/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingSingleLinkList.node
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+   
